8e2e41ceaa64326026d53e37939a2591f46dcb4d
Ascani Carlo <carlotm@protonmail.com>
2024-02-17 10:54:18 +0100

    exit with the right exit code

--- elmex/lib/mix/tasks/elm.compile.ex
+++ elmex/lib/mix/tasks/elm.compile.ex
@@ -4,13 +4,23 @@ defmodule Mix.Tasks.Elm.Compile do
 
   def run(_) do
     Elmex.start()
+
     Elmex.compile()
-    |> Enum.group_by(fn(rc) -> rc == 0 end)
+    |> Enum.group_by(fn rc -> rc == 0 end)
     |> Enum.map(fn
-      ({true, l}) -> "Success: #{length(l)}" 
-      ({_, l}) -> "Failed: #{length(l)}" 
+      {true, l} -> "Success: #{length(l)}"
+      {_, l} -> "Failed: #{length(l)}"
     end)
     |> Enum.join(" - ")
-    |> IO.puts()
+    |> then(fn out ->
+      rc =
+        case String.contains?(out, "Failed") do
+          true -> {:shutdown, 1}
+          _ -> :normal
+        end
+
+      IO.puts(out)
+      exit(rc)
+    end)
   end
 end

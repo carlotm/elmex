028cea285059237ddea300c393289f3bcb224852
Ascani Carlo <carlotm@protonmail.com>
2024-02-19 12:37:54 +0100

    Fix elmex dep path in demo

--- demo/mix.exs
+++ demo/mix.exs
@@ -55,7 +55,7 @@ defmodule Demo.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:elmex, path: "../elmex"}
+      {:elmex, path: "../"}
     ]
   end
 

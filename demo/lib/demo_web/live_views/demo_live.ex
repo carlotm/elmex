af2eb1360d63727d4b653c1d5de721f1c0b825be
Ascani Carlo <carlotm@protonmail.com>
2024-02-22 15:38:43 +0100

    Format template

--- demo/lib/demo_web/live_views/demo_live.ex
+++ demo/lib/demo_web/live_views/demo_live.ex
@@ -5,8 +5,11 @@ defmodule DemoWeb.DemoLive do
     ~H"""
     <div class="p-4 space-y-2">
       <h1 class="font-bold text-2xl">Elmex demo</h1>
+
       <p class="rounded p-2 bg-slate-200" :if={@from_elm}>Elm says: <%= @from_elm %></p>
+
       <p>Some Elm apps are rendered below to demostrate Elmex.</p>
+
       <h2 class="font-bold text-xl">Flags and ports</h2>
       <div
         phx-hook="Elmex"
@@ -15,16 +18,19 @@ defmodule DemoWeb.DemoLive do
         data-flags="42"
         data-ports="true"
       />
+
       <h2 class="font-bold text-xl">Counter</h2>
       <div phx-hook="Elmex" id="Counter" data-elm-app="Counter" />
+
       <h2 class="font-bold text-xl">Text reverse</h2>
       <div phx-hook="Elmex" id="TextReverse" data-elm-app="TextReverse" />
+
       <button
         class="p-2 bg-yellow-200 rounded"
         phx-click="talk-to-elm"
         value="Elm is cool"
       >
-        Click me!!!
+        Send an event to elm!!!
       </button>
     </div>
     """
